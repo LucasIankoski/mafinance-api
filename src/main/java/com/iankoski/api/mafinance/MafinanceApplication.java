65598b83e7730f990dcf196ee5da904e2ce5b592
Lucas Iankoski <lucas@digihub.com.br>
2025-04-04 08:16:53 -0300

    first commit

---
+++ src/main/java/com/iankoski/api/mafinance/MafinanceApplication.java
@@ -0,0 +1,13 @@
+package com.iankoski.api.mafinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MafinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iankoski/api/mafinance/MafinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iankoski.api.mafinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MafinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
