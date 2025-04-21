12a7f3beaa63b5d2fc629cc568347cdda23d53b8
Lucas Iankoski <lucas@digihub.com.br>
2025-04-20 23:16:07 -0300

    refact(mafinance): refatorando a classe Agent para atribuir o Lombok.

--- src/main/java/com/iankoski/api/mafinance/entity/Agent.java
+++ src/main/java/com/iankoski/api/mafinance/entity/Agent.java
@@ -1,10 +1,16 @@
 package com.iankoski.api.mafinance.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "agent")
 public class Agent {
 
@@ -27,53 +33,4 @@ public class Agent {
 
     @Column(name = "active")
     private boolean active;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
 }
